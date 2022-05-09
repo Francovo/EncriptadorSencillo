60f11a05773151c5f4004810102c64a470964391
Franco <francovelasco10hotmail.com>
2022-05-09 11:43:39 -0500

    Finally Update

--- Encriptador.js
+++ Encriptador.js
@@ -3,12 +3,50 @@ let Result;
 
 const GetDataSubmit = () => {
   Data = document.querySelector("#input1").value;
-  console.log(Data);
-  Data.slice(4)
-  Result = Data.replace(/a/gi, "ai")
-    .replace(/e/gi, "enter")
-    .replace(/i/gi, "imes")
-    .replace(/o/gi, "ober")
-    .replace(/u/gi, "ufat");
-  console.log(Result);
+
+  Result = Array.from(Data).map((char) => {
+    if (char === "a") {
+      return char.replace(char, `${char}i`);
+    } else if (char === "e") {
+      return char.replace(char, `${char}nter`);
+    } else if (char === "i") {
+      return char.replace(char, `${char}mes`);
+    } else if (char === "o") {
+      return char.replace(char, `${char}ber`);
+    } else if (char === "u") {
+      return char.replace(char, `${char}enter`);
+    }
+    return char;
+  });
+  let x = Result.join("");
+  console.log(x);
+
+  if (Data === "") {
+    document.getElementById("CardBack").innerHTML = `
+    <img class="Image" src="./images/Muñeco.svg" />
+    <h3 style="word-break: break-all">Ningun mensaje fue encontrado</h3>
+    <h6>Ingresa el texto que deseas encriptar o desencriptar</h6>
+    <h1>Developed by Franco Velasco</h1>
+    `;
+  } else {
+    document.getElementById("CardBack").innerHTML = `
+    <h1>${x}</h1>
+    `;
+  }
+};
+
+const DesEncriptar = () => {
+  Dato = Result;
+
+  Resultado = Data.replace(/ai/gi, "a")
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  console.log(Resultado);
+
+
+  document.getElementById("CardBack").innerHTML = `
+  <h1>${Resultado}</h1>
+  `;
 };
